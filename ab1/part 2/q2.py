ecd1ae190c183dbf1936954c02512b35cb228bfd
Kleber Vasconcelos <klebervasconcelos@MacBook-Pro-de-Kleber.local>
2023-09-13 15:50:07 -0300

    Q2 update

--- ab1/part 2/q2.py
+++ ab1/part 2/q2.py
@@ -1,3 +1,43 @@
 from setup import * 
 
-ets_3d()
+import numpy as np
+import matplotlib.pyplot as plt
+
+from spatialmath.base import *
+def htransform_3d():
+    """
+    Matriz de transformação homogênea 3D.
+    """
+    T0 = transl(0, 0, 0)
+    # T = transl(3, 0, 0) @ trotx(np.pi / 2) @ transl(0, 4, 0)
+    # P = transl(1, 3, 1)
+
+    trplot(T0, frame="O", color="k")
+    # trplot(T, frame="A", color="b")
+    # trplot(P, frame="P", color="r", length=0, axislabel=False)
+    
+    print(T0)
+
+    plt.show()
+
+
+def ets_3d2():
+    """
+    Cria um manipulador espacial do tipo RRRRRR.
+    """
+    print("# --- Exemplo simples ETS 3D --- #")
+    a_1 = 1
+    a_2 = 1
+    a_3 = 0.5
+    
+    robot3 = ERobot(
+        ET.tz(a_1) * ET.Rz() * ET.Ry() * ET.tz(a_2) * ET.Ry()  * ET.tz(a_3) 
+    )
+
+    print(f"Fkine =\n{robot3.fkine(np.deg2rad([30, 40, 50]))}")
+    print(f"# Joints: {robot3.n}")
+    print(f"Structure: {robot3.structure}")
+
+    robot3.teach(q=np.deg2rad([30, 40, 50]))
+    
+ets_3d2()
